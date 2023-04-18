35749c4e5e9216df18e1770907142794a7b13de3
irvanmaulana2100 <101796034+irvanmaulana2100@users.noreply.github.com>
2023-04-19 05:36:45 +0700

    Add files via upload

--- estimasi_mobil.py
+++ estimasi_mobil.py
@@ -1,11 +1,20 @@
 import pickle
 import streamlit as st
+from PIL import Image
 
 model = pickle.load(open('estimasi_mobil.sav', 'rb'))
 
-st.title('Selamat Datang di Website Estimasi Harga Mobil Toyota Bekas')
+st.title('Estimasi Harga Mobil Toyota Bekas')
+st.image('toyota.png')
+st.text('Silahkan diisi dengan benar.')
 
-year = st.number_input('Input Tahun Pembuatan')
+st.sidebar.header('Kritik dan Saran')
+st.sidebar.text_input('Nama')
+st.sidebar.text_input('Email')
+st.sidebar.text_area('Kritik & Saran')
+st.sidebar.button('KIRIM')
+
+year = st.number_input('Masukkan Tahun Pembuatan')
 mileage = st.number_input('Masukkan Kilometer Mobil')
 tax = st.number_input('Masukkan Pajak Mobil')
 mpg = st.number_input('Masukkan Konsumsi BBM')
@@ -17,5 +26,4 @@ if st.button('Estimasi Harga'):
     predict = model.predict(
         [[year, mileage, tax, mpg, engineSize]]
     )
-    st.write('Estimasi Harga Mobil dalam Pounds : ', predict)
-    st.write('Estimasi Harga Mobil dalam IDR(Juta) : ', predict*18500)
+    st.write('Estimasi Harga Mobil dalam Rupiah(Juta) : ', predict*18500)
